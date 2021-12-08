eca2c364cc888a0429a7ac1df052095817f32b92
Nevis4 <przemyslaw.malara@icloud.com>
2021-12-09 00:42:38 +0100

    Create begin landing page with app navbar and app home

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  
+})
+export class NavbarComponent implements OnInit {
+ 
+  showBtnMenu(btn: any){
+  
+  btn.classList.toggle('show')
+  }
+ 
+  constructor() { }
+ 
+  ngOnInit(): void {
+  }
+
+}
